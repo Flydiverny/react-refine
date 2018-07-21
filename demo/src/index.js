e29f250ca1341dfbba4271733e8c129b99580e4d
Markus Maga <markus@nadilus.com>
2018-07-21 02:09:40 +0200

    Fix demo and prettier

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from "react";
-import ReactDOM from "react-dom";
+import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
 
 import {
   RefineScope,
@@ -9,17 +9,15 @@ import {
   filterController,
   sortController,
   SORT_MODE_ONE,
-  SORT_MODE_TIE
-} from "../../src";
-import { PRIORITY_FIRST } from "../../src/RefineScope";
+  SORT_MODE_TIE,
+  PRIORITY_FIRST,
+} from '../../src';
 
-const items = ["Hello World", "12345", "Hello W0r1d", "66666", "55555"];
+const items = ['Hello World', '12345', 'Hello W0r1d', '66666', '55555'];
 
-const freeTextFilter = filter => items =>
-  items.filter(item => item.includes(filter));
+const freeTextFilter = filter => items => items.filter(item => item.includes(filter));
 
-const lengthFilter = filter => items =>
-  items.filter(item => item.length <= filter);
+const lengthFilter = filter => items => items.filter(item => item.length <= filter);
 
 const onChange = (filter, setFilter, clearFilter) => evt => {
   if (evt.target.value.length === 0) {
@@ -36,9 +34,9 @@ const LengthFilter = ({ setFilter, clearFilter }) => (
   <input onChange={onChange(lengthFilter, setFilter, clearFilter)} />
 );
 
-const Sorter = ({ toggleSorter, removeSorter, sortMode }) => (
+const Sorter = ({ toggleSorter, removeSorter, sortDirection }) => (
   <Fragment>
-    {sortMode}
+    {sortDirection}
     <button onClick={toggleSorter}>Toggle</button>
     <button onClick={removeSorter}>Off</button>
   </Fragment>
@@ -47,9 +45,7 @@ const Sorter = ({ toggleSorter, removeSorter, sortMode }) => (
 const EnhancedFreeTextFilter = filterController(FreeTextFilter);
 const EnhancedLengthFilter = filterController(LengthFilter);
 const LengthSorter = sortController((a, b) => a.length - b.length)(Sorter);
-const AlphabeticSorter = sortController((a, b) => (a < b ? -1 : a > b ? 1 : 0))(
-  Sorter
-);
+const AlphabeticSorter = sortController((a, b) => (a < b ? -1 : a > b ? 1 : 0))(Sorter);
 
 const Facet = ({ component: Component, ...props }) => (
   <table>
@@ -115,4 +111,4 @@ class Demo extends Component {
   }
 }
 
-ReactDOM.render(<Demo />, document.querySelector("#demo"));
+ReactDOM.render(<Demo />, document.querySelector('#demo'));
