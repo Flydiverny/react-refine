85b596698c4bcdab6445f22808e3f231c52fb84e
Markus Maga <markus@nadilus.com>
2018-07-22 23:24:17 +0200

    Refactor

--- src/Refine.js
+++ src/Refine.js
@@ -2,27 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import RefineContext from './RefineContext';
 
-const consumerPropTypes = {
-  items: PropTypes.array.isRequired,
-  children: PropTypes.func.isRequired,
-};
-
-const funnel = op => ({ items, children }) => (
+const facet = op => ({ items, children }) => (
   <RefineContext.Consumer>
     {({ [op]: operation }) => children(operation(items))}
   </RefineContext.Consumer>
 );
 
-const Refine = funnel('refine');
-Refine.propTypes = consumerPropTypes;
-Refine.displayName = 'Refine';
+export const [Refine, Filter, Sort] = ['Refine', 'Filter', 'Sort'].map(type => {
+  const Facet = facet(type.toLowerCase());
+  Facet.propTypes = {
+    items: PropTypes.array.isRequired,
+    children: PropTypes.func.isRequired,
+  };
+  Facet.displayName = type;
 
-const Filter = funnel('filter');
-Filter.propTypes = consumerPropTypes;
-Filter.displayName = 'Filter';
-
-const Sort = funnel('sort');
-Sort.propTypes = consumerPropTypes;
-Sort.displayName = 'Sort';
-
-export { Refine, Filter, Sort };
+  return Facet;
+});
